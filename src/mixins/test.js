bc11304b32d038fc05d23f47d2abea53fa5b1822
huangzhihua <huangzhihua@jd.com>
2018-01-26 20:34:51 +0800

    static page

--- src/mixins/test.js
+++ src/mixins/test.js
@@ -2,7 +2,8 @@ import wepy from 'wepy'
 
 export default class testMixin extends wepy.mixin {
   data = {
-    mixin: 'This is mixin data.'
+    mixin: 'This is mixin data.',
+    pixelRatio: 2
   }
   methods = {
     tap () {
@@ -15,7 +16,31 @@ export default class testMixin extends wepy.mixin {
     console.log('mixin onShow')
   }
 
+  onReady() {
+    console.log('onready')
+  }
   onLoad() {
-    console.log('mixin onLoad')
+  }
+
+  rpx2px() {
+    console.log('rpx2px')
+  }
+
+  initDevice () {
+    let $root = this.getRoot()
+    this.pixelRatio = $root.systemInfo.pixelRatio
+  }
+
+  getRoot() {
+    let root = this.$parent
+    while (root.$parent) {
+      root = root.$parent
+    }
+    return root
+  }
+
+  getImg(path, suffix) {
+    let size = this.pixelRatio <= 1 ? '' : (this.pixelRatio >= 3 ? '@3x' : '@2x')
+    return `${path}${size}.${suffix}`
   }
 }
